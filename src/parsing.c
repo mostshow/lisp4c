15ba8edf2ed18b6eaacf8fb382f1b840c8783d41
lyndon <mostshow@users.noreply.github.com>
2017-07-05 11:15:06 +0800

    use mpc

--- src/parsing.c
+++ src/parsing.c
@@ -2,28 +2,26 @@
 
 int main(int argc, char** argv) {
 
+  mpc_parser_t* Adjective = mpc_new("adjective");
+  mpc_parser_t* Noun      = mpc_new("noun");
+  mpc_parser_t* Phrase    = mpc_new("phrase");
+  mpc_parser_t* Doge      = mpc_new("doge");
 
-  mpc_parser_t* Adjective = mpc_or(4, 
-    mpc_sym("wow"), mpc_sym("many"),
-    mpc_sym("so"),  mpc_sym("such")
-  );
-
-
-  mpc_parser_t* Noun = mpc_or(5,
-    mpc_sym("lisp"), mpc_sym("language"),
-    mpc_sym("book"), mpc_sym("build"), 
-    mpc_sym("c")
-  );
-
-  mpc_parser_t* Phrase = mpc_and(2, mpcf_strfold, 
-    Adjective, Noun, free);
-
-  mpc_parser_t* Doge = mpc_many(mpcf_strfold, Phrase);
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                           \
+      adjective : \"wow\" | \"many\"            \
+                |  \"so\" | \"such\";           \
+      noun      : \"lisp\" | \"language\"       \
+                | \"book\" | \"build\" | \"c\"; \
+      phrase    : <adjective> <noun>;           \
+      doge      : <phrase>*;                    \
+    ",
+    Adjective, Noun, Phrase, Doge);
 
 
-  mpc_delete(Doge);
-
+  mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+  
   return 0;
-
+  
 }
 // cc -std=c99 -Wall parsing.c mpc.c -ledit -lm -o parsing
