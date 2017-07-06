3815d5c6f0a20a4ca6cd12ab8fd6bfd6fb770285
lyndon <mostshow@users.noreply.github.com>
2017-07-06 11:54:41 +0800

    parse float

--- src/prompt.c
+++ src/prompt.c
@@ -24,18 +24,17 @@ void add_history(char* unused) {}
 #endif
 
 
-
 int main(int argc, char** argv) {
 
 	mpc_parser_t *Number = mpc_new("number");
 	mpc_parser_t *Operator = mpc_new("operator");
 	mpc_parser_t *Expr = mpc_new("expr");
 	mpc_parser_t *Lispy = mpc_new("lispy");
-
+ 
 	mpca_lang(MPCA_LANG_DEFAULT, 
 		"												\
-			number : /-?[0-9]+/ ; 						\
-			operator : '+' | '-' | '*' | '/'; 			\
+			number : /-?[0-9]*\\.?[0-9]+/; 				\
+			operator : '+' | '-' | '*' | '/'  ;			\
 			expr : <number> | '(' <operator><expr>+ ')';\
 			lispy : /^/ <operator><expr>+ /$/;			\
 		",												
